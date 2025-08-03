737def0263bde97218e863d2530b0c45574c859c
Alexandre <alexandretibes9@gmail.com>
2025-08-03 14:09:21 -0300

    feat: adiciona o export do Firestore para ser usado no App.jsx

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Configuração do Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyASIN01StEyruTUpiHlBfiF1zNfGXwxLgY",
   authDomain: "chapterkeeper-786b3.firebaseapp.com",
   projectId: "chapterkeeper-786b3",
   storageBucket: "chapterkeeper-786b3.firebasestorage.app",
   messagingSenderId: "37697630073",
-  appId: "1:37697630073:web:42be28e6bdd42f705849a8"
+  appId: "1:37697630073:web:42be28e6bdd42f705849a8",
 };
 
-// Initialize Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
